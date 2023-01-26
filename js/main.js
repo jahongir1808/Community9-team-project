eb5b9de6bd690972db969e32c91279ac20a65f46
jahongir1808 <jahongir.developer18@gmail.com>
2023-01-26 10:48:45 +0500

    variables added

--- js/main.js
+++ js/main.js
@@ -31,3 +31,45 @@ function includeHTML() {
 }
 
 includeHTML();
+
+let backtop = document.getElementById("backtop");
+
+function toggleBacktop() {
+  if (
+    document.body.scrollTop > 200 ||
+    document.documentElement.scrollTop > 200
+  ) {
+    backtop.style.opacity = 1;
+  } else {
+    backtop.style.opacity = 0;
+  }
+}
+
+window.addEventListener("scroll", function () {
+  toggleBacktop();
+});
+
+window.addEventListener("scroll", function () {
+  shrink();
+});
+
+let navbar = document.getElementById("navbar");
+
+function shrink() {
+  if (
+    document.body.scrollTop > 100 ||
+    document.documentElement.scrollTop > 100
+  ) {
+    navbar.classList.add("navbar-shrink");
+  } else {
+    navbar.classList.remove("navbar-shrink");
+  }
+}
+
+window.addEventListener("load", () => {
+  document.getElementById("loading").style.display = "none";
+});
+// or
+setTimeout(() => {
+  document.getElementById("loading").style.display = "none";
+}, 2000);
